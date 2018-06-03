0f5b393a94edefbea9456342d6ad324e70ae0c86
Knightzhy <wuzhy.dk@gmail.com>
2018-06-03 08:49:09 -0400

    add client

---
+++ sample_client/echo_client.cpp
@@ -0,0 +1,36 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main()
+{
+    // create socket
+    int socket_fd;
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0 ) {
+        printf("When create socket, there is a wrong. errno:%d\n", socket_fd);
+        return 0;
+    }
+    printf("create socket success.[%d]\n", socket_fd);
+
+    // connet
+    sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(5555);
+    inet_pton(AF_INET, "0.0.0.0", &server_addr.sin_addr);
+    int ret = connect(socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if (ret < 0) {
+        printf("When connect, there is a wrong. errno:%d\n", ret);
+        return 0;
+    }
+    printf("connect success.\n");
+    shutdown(socket_fd, 2);
+    return 0;
+}

d6bf3e1d89efce33122370b725bc2ce8ae5a7bdf
Knightzhy <wuzhy.dk@gmail.com>
2018-09-27 19:14:05 -0400

    add readn

--- sample_io/test_file.cpp
+++ sample_io/test_file.cpp
@@ -33,11 +33,8 @@ TEST(IOTest, TWriten)
     int fd = open("aa", O_CREAT | O_APPEND | O_RDWR, S_IRUSR | S_IWUSR);
     ASSERT_GT(fd, 0);
     printf("fd=%d.\n", fd);
-    char *v = new char[100000000];
-    for (int i=0; i<100000001; i++) {
-        v[i] = 'a';
-    }
-    v[100000000] = '\0';
+    char *v = new char[100]();
+    strncpy(v, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 26);
     ssize_t n = writen(fd, v, strlen(v));
     ASSERT_EQ(n, strlen(v));
     delete []v;
@@ -48,9 +45,42 @@ TEST(IOTest, TWriten)
 // ssize_t read(int fd, void *buf, size_t nbytes);
 ssize_t readn(int fd, void *buf, size_t n)
 {
+    char *ptr;
+    ptr = (char *)buf;
+    size_t nleft = n;
+    ssize_t nreadn;
+    while (nleft>0) {
+        nreadn = read(fd, ptr, nleft);
+        printf("read return nreadn=%d\n", (int)nreadn);
+        if (nreadn <0) {
+            if (errno == EINTR) {
+                nreadn = 0;
+            } else {
+                printf("errno=%d,error=%s.\n", errno, strerror(errno));
+                return -1;
+            }
+        }
+        if (nreadn ==0) {
+            break;
+        }
+        nleft -= nreadn;
+        ptr += nreadn;
+    }
+    return n - nleft;
 }
 
-
+TEST(IOTest, TReadn)
+{
+    int fd = open("aa", O_RDONLY);
+    printf("fd=%d\n", fd);
+    char *v = new char[200]();
+    ssize_t n = readn(fd, v, 200);
+    printf("readn return n=%d.\n", (int)n);
+    printf("v.size=%d.\n", strlen(v));
+    printf("v=%s.\n", v);
+    delete []v;
+    close(fd);
+}
 
 TEST(IOTest, TCreat)
 {

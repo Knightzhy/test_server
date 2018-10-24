9426e596c73a89817f2d6eded125d59bc987c340
Knightzhy <wuzhy.dk@gmail.com>
2018-10-24 14:06:24 -0400

    xx

--- sample_thread/test_thread.cpp
+++ sample_thread/test_thread.cpp
@@ -2,9 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
-void *func(void*)
+#include <string>
+#include <stdlib.h>
+
+struct member{
+    char* name;
+    uint16_t age;
+};
+void *func(void* m)
 {
+    sleep(2);
     printf("create thread id=%u\n", (uint32_t)pthread_self());
+    member *a = (struct member*)m;
+    printf("m.name=%s, m.age=%d\n", a->name, a->age);
     return NULL;
 }
 
@@ -13,9 +23,18 @@ int main()
 {
     pthread_t tid;
     int ret;
-    ret = pthread_create(&tid, NULL, func, NULL);
+    member *a = (struct member *)malloc(sizeof(struct member));
+    if (a==NULL){
+        printf("malloc error\n");
+        return 0;
+    }
+    a->name = "HH";
+    a->age = 16;
+    printf("m.name=%s, m.age=%d\n", a->name, a->age);
+    ret = pthread_create(&tid, NULL, func, a);
     printf("ret=%d, thread id=%u\n", ret, (uint32_t)pthread_self());
     ret = pthread_join(tid, NULL);
     printf("join ret=%d\n", ret);
+    free(a);
     return 0;
 }

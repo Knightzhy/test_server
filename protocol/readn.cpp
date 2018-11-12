8c57a26ed02b0ee4cbc8ef22d4bd15134f0a080a
Knightzhy <wuzhy.dk@gmail.com>
2018-11-12 02:36:59 -0500

    fixbug

--- protocol/readn.cpp
+++ protocol/readn.cpp
@@ -1,5 +1,7 @@
 #include <unistd.h>
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 ssize_t readn(int fd, void *vptr, size_t n)
 {
@@ -10,6 +12,14 @@ ssize_t readn(int fd, void *vptr, size_t n)
     nleft = n;
     while (nleft > 0) {
         if ( (nread = read(fd, ptr, nleft)) < 0) {
+            if (errno == EINTR 
+             || errno == EWOULDBLOCK 
+             || errno == EAGAIN) {
+                break;
+            }
+
+            printf("error, return nread=%d, errno=%d. error=%s\n",
+                    nread, errno, strerror(errno));
             return -1;
         }
         printf("read xx=%d\n", nread);

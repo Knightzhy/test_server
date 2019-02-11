1b7e874052941e9f2442201296fd10be6d065b47
Knightzhy <Wuzhy.dk@gmail.com>
2019-02-12 02:58:17 +0800

    xx

--- sample_signal/test_signal.cpp
+++ sample_signal/test_signal.cpp
@@ -1,43 +1,73 @@
-#include <stdio.h>
-#include <signal.h>
-#include <stdlib.h>
+#include <signal.h>  
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <error.h>  
+#include <string.h>  
 #include <unistd.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <dlfcn.h>
-#include "func.h"
-
-void func1(int no)
-{
-    printf("yy\n");
-    return;
-}
-
-typedef void (*CAC_FUNC)(int);
-int main(int argc, char *argv[])
-{
-    while(1) {
-        void (*f)(int);
-        void *dl_handle = dlopen("libfunc.so", RTLD_LAZY);
-        if (!dl_handle) {
-            printf( "!!! %s\n", dlerror() );
-            return 0;
-        }
-        f = (CAC_FUNC)dlsym(dl_handle, "func");
-        char *err = dlerror();
-        if (err != NULL) {
-            printf( "!!! %s\n", err);
-            return 0;
-        }
-
-        signal(SIGINT, f);
-        signal(SIGFPE, f);
-        if(dlclose(dl_handle)!=0) {
-            printf("dlclose error.\n");
-            return 0;
-        }
-        sleep(1);
-    }
-//    int b = 10 / 0;
-    return 0;
-}
+  
+/* 版本1, 可靠信号将被递送多次 */  
+//#define MYSIGNAL SIGRTMIN+5  
+/* 版本2, 不可靠信号只被递送一次 */  
+#define MYSIGNAL SIGTERM  
+  
+void sig_handler(int signum)  
+{  
+    psignal(signum, "catch a signal");  
+}  
+  
+int main(int argc, char **argv)  
+{  
+    sigset_t block, pending;  
+    int sig, flag;  
+  
+    /* 设置信号的handler */  
+    signal(MYSIGNAL, sig_handler);  
+  
+    /* 屏蔽此信号 */  
+    sigemptyset(&block);  
+    sigaddset(&block, MYSIGNAL);  
+    printf("block signal\n");  
+    sigprocmask(SIG_BLOCK, &block, NULL);  
+  
+    /* 发两次信号, 看信号将会被触发多少次 */  
+    printf("---> send a signal --->\n");  
+    kill(getpid(), MYSIGNAL);  
+    printf("---> send a signal --->\n");  
+    kill(getpid(), MYSIGNAL);  
+    printf("---> send a signal --->\n");  
+    kill(getpid(), MYSIGNAL);  
+  
+    /* 检查当前的未决信号 */  
+    flag = 0;  
+    sigpending(&pending);  
+    for (sig = 1; sig < NSIG; sig++) {  
+        if (sigismember(&pending, sig)) {  
+            printf("signal:%d\n", sig);
+            flag = 1;  
+            psignal(sig, "this signal is pending");  
+        }   
+    }  
+    if (flag == 0) {  
+        printf("no pending signal\n");  
+    }  
+  
+    /* 解除此信号的屏蔽, 未决信号将被递送 */  
+    printf("unblock signal\n");  
+    sigprocmask(SIG_UNBLOCK, &block, NULL);  
+  
+    /* 再次检查未决信号 */  
+    flag = 0;  
+    sigpending(&pending);  
+    for (sig = 1; sig < NSIG; sig++) {  
+        if (sigismember(&pending, sig)) {  
+            printf("signal:%d\n", sig);
+            flag = 1;  
+            psignal(sig, "a pending signal");  
+        }   
+    }  
+    if (flag == 0) {  
+        printf("no pending signal\n");  
+    }  
+  
+    return 0;  
+}  
